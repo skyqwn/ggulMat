b7badb285c3c7dd02aea84c4aa22a4f62f34f05a
kimba <skyqwn@gmail.com>
2024-09-30 17:17:03 +0900

    Geolocation을 사용한 내위치 버튼추가

--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -4,17 +4,46 @@ import MapHomeScreen from '@/screens/map/MapHomeScreen';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import MapStackNavigator, {MapStackParamList} from '../stack/MapStackNavigator';
+import {mainNavigations} from '@/constants';
+import {NavigatorScreenParams} from '@react-navigation/native';
 
-interface MainDrawerNavigatorProps {}
+export type MainDrawerParamList = {
+  [mainNavigations.HOME]: NavigatorScreenParams<MapStackParamList>;
+  [mainNavigations.FEED]: undefined;
+  [mainNavigations.CALENDAR]: undefined;
+};
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator<MainDrawerParamList>();
 
-function MainDrawerNavigator({}: MainDrawerNavigatorProps) {
+function MainDrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="MapHome" component={MapHomeScreen} />
-      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
-      <Drawer.Screen name="Calendar" component={CalendarScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'front',
+      }}>
+      <Drawer.Screen
+        name={mainNavigations.HOME}
+        component={MapStackNavigator}
+        options={{
+          title: '홈',
+        }}
+      />
+      <Drawer.Screen
+        name={mainNavigations.FEED}
+        component={FeedHomeScreen}
+        options={{
+          title: '피드',
+        }}
+      />
+      <Drawer.Screen
+        name={mainNavigations.CALENDAR}
+        component={CalendarScreen}
+        options={{
+          title: '달력',
+        }}
+      />
     </Drawer.Navigator>
   );
 }
--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,25 +1,120 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import MapView, {LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import Geolocation from '@react-native-community/geolocation';
 
-import useAuth from '@/hooks/queries/useAuth';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {colors} from '@/constants';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+
+type Navigation = CompositeNavigationProp<
+  StackNavigationProp<MapStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
 
 function MapHomeScreen() {
-  const {logoutMutation} = useAuth();
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516,
+    longitude: 126.9898,
+  });
+  const [isUserLocationError, setIsUserLocationError] = useState(false);
+
+  const inset = useSafeAreaInsets();
+  const navigation = useNavigation<Navigation>();
+  const mapRef = useRef<MapView | null>(null);
+
+  const handlePressUserLocation = () => {
+    if (isUserLocationError) {
+      //todo 에러메세지표시
+      return;
+    }
+    mapRef.current?.animateToRegion({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421, // 확대의 정도
+    });
+  };
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => {
+        const {latitude, longitude} = info.coords;
+        setUserLocation({latitude, longitude});
+        setIsUserLocationError(false);
+      },
+      () => {
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true, //좀더 정확한 위치를 잡을 수있게함
+      },
+    );
+  }, []);
+
   return (
-    <MapView
-      style={styles.container}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      followsUserLocation
-      showsMyLocationButton
-    />
+    <>
+      <MapView
+        ref={mapRef}
+        style={styles.container}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={false}
+      />
+      <Pressable
+        style={[styles.drawerButton, {top: inset.top || 20}]}
+        onPress={() => navigation.openDrawer()}>
+        <Text>서랍</Text>
+      </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <Text>내위치</Text>
+        </Pressable>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  drawerButton: {
+    position: 'absolute',
+    left: 0,
+    top: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: colors.GREEN_500,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.5,
+    elevation: 4, // 안드로이드 그림자속성
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15,
+  },
+  mapButton: {
+    backgroundColor: colors.GREEN_500,
+    marginVertical: 5,
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2, // 안드로이드 그림자속성
   },
 });
 
